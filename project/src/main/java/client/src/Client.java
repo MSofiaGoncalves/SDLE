42de0b7a484150db8d107b53d714bd4f5f364ef9
Maria Sofia Gonçalves <80844884+MSofiaGoncalves@users.noreply.github.com>
2023-10-23 21:53:29 +0100

    adding zmq client

--- project/src/main/java/client/src/Client.java
+++ project/src/main/java/client/src/Client.java
@@ -1,14 +1,51 @@
 package client.src;
+/*package client.src;
 
-//import states.HubState;
-//import states.State;
+import states.HubState;
+import states.State;
 
 public class Client {
     public static void main(String[] args) {
-        /*State state = new HubState();
+        State state = new HubState();
 
         while (state != null) {
             state = state.step();
-        }*/
+        }
+    }
+}*/
+
+
+//
+//  Hello World client in Java
+//  Connects REQ socket to tcp://localhost:5555
+//  Sends "Hello" to server, expects "World" back
+//
+
+import org.zeromq.ZMQ;
+import org.zeromq.ZContext;
+
+public class Client
+{
+    public static void main(String[] args)
+    {
+        try (ZContext context = new ZContext()) {
+            //  Socket to talk to server
+            System.out.println("Connecting to hello world server");
+
+            ZMQ.Socket socket = context.createSocket(ZMQ.REQ);
+            socket.connect("tcp://localhost:5555");
+
+            for (int requestNbr = 0; requestNbr != 10; requestNbr++) {
+                String request = "Hello";
+                System.out.println("Sending Hello " + requestNbr);
+                socket.send(request.getBytes(ZMQ.CHARSET), 0);
+
+                byte[] reply = socket.recv(0);
+                System.out.println(
+                        "Received " + new String(reply, ZMQ.CHARSET) + " " +
+                                requestNbr
+                );
+            }
+        }
     }
 }
