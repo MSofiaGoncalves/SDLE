8c20747dfa2171f96701ad782f422fea430cfb1c
Maria Sofia Gonçalves <80844884+MSofiaGoncalves@users.noreply.github.com>
2023-10-24 08:45:19 +0100

    added client from Bea

---
+++ projetoooo/src/main/java/client/model/ShoppingList.java
@@ -0,0 +1,153 @@
+package client.model;
+
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.*;
+
+public class ShoppingList {
+    private String id;
+    private String name;
+    private Map<String, Integer> products;
+
+    public ShoppingList(String id, String name) {
+        this.id = id;
+        this.name = name;
+        this.products = new HashMap<>();
+    }
+
+    public ShoppingList(String id, String name, Map<String, Integer> products) {
+        this.id = id;
+        this.name = name;
+        this.products = products;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Map<String, Integer> getProducts() {
+        return products;
+    }
+
+    public void addProduct(String name, int quantity) {
+        if (products.containsKey(name)) {
+            System.out.println("exists");
+            addQuantity(name, quantity);
+        } else {
+            products.put(name, quantity);
+        }
+    }
+
+    public void removeProduct(String name, int quantity) {
+        products.remove(name);
+    }
+
+    public void addQuantity(String name, int quantity) {
+        Integer currQuantity = products.get(name);
+        System.out.println("Cur quantity: " + currQuantity);
+        System.out.println("New quantity: " + (currQuantity + quantity));
+        products.put(name, currQuantity + quantity);
+    }
+
+    public void removeQuantity(String name, int quantity) {
+        Integer currQuantity = products.get(name);
+        if (currQuantity - quantity > 1) {
+            products.put(name, currQuantity - quantity);
+        } else {
+            products.remove(name);
+        }
+    }
+
+    public void printProducts() {
+        System.out.println("\t* Product: Quantity ");
+        for (Map.Entry<String, Integer> entry : products.entrySet()) {
+            System.out.println("\t" + entry.getKey() + ": " + entry.getValue());
+        }
+        System.out.println("\n");
+    }
+
+    public Boolean hasProducts() {
+        return this.products.size() != 0;
+    }
+
+//    public JSONObject toJSonObject() {
+//        JSONObject shoppingListJson = new JSONObject();
+//        shoppingListJson.put("name", name);
+//        shoppingListJson.put("id", id);
+//
+//        JSONArray productsArray = new JSONArray();
+//        for (Map.Entry<String, Integer> entry : products.entrySet()) {
+//            JSONObject productJson = new JSONObject();
+//            productJson.put("name", entry.getKey());
+//            productJson.put("quantity", entry.getValue());
+//            productsArray.add(productJson);
+//        }
+//
+//        shoppingListJson.put("products", productsArray);
+//        return shoppingListJson;
+//    }
+//
+//    public void saveToFile() {
+//        String fileName = "client/lists/" + id + ".json";
+//        JSONObject jsonObject = toJSonObject();
+//
+//        try (FileWriter file = new FileWriter(fileName)) {
+//            file.write(jsonObject.toJSONString());
+//            System.out.println("Shopping list has been written to " + fileName);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+    // TODO: Function that reads from json file
+    public static ShoppingList loadFromFile(String fileName){
+//        System.out.println(fileName);
+//        JSONParser parser = new JSONParser();
+//        //String fileDirectory = "client/lists/" + fileName;
+//
+//        try (FileReader reader = new FileReader(fileName)) {
+//            JSONObject jsonShoppingList = (JSONObject) parser.parse(reader);
+//
+//            String listID = (String) jsonShoppingList.get("id");
+//            String name = (String) jsonShoppingList.get("name");
+//
+//            Map<String, Integer> fileProducts = new HashMap<>();
+//            JSONArray productsArray = (JSONArray) jsonShoppingList.get("products");
+//            for (Object product : productsArray) {
+//                JSONObject productJson = (JSONObject) product;
+//                String productName = (String) productJson.get("name");
+//                System.out.println(productName);
+//                long productQuantity = (long) productJson.get("quantity");
+//                //productJson.
+//                fileProducts.put(productName, (int) productQuantity);
+//            }
+//
+//            return new ShoppingList(name, listID, fileProducts);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (ParseException e) {
+//            throw new RuntimeException(e);
+//        }
+//        return null;
+
+        try (Reader reader = new FileReader(fileName)) {
+            // Create a Gson object
+            Gson gson = new Gson();
+
+            // Parse the JSON file into a ShoppingList object
+            ShoppingList shoppingList = gson.fromJson(reader, ShoppingList.class);
+
+            return shoppingList;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- projetoooo/src/main/java/client/src/model/List.java
+++
@@ -1,9 +0,0 @@
-package client.src.model;
-
-public class List {
-    private String id;
-
-    public List(String id) {
-        this.id = id;
-    }
-}
